8e30996016ba72d09a87de47267d25d5ac96bcb0
joel-samaniego <joel.samaniego@university.mentormate.com>
2023-06-25 14:30:54 -0400

    Add - New DropDown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-scroll";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Night from "./Night";
 import Carousel from "./Carousel";
+import Example from "./Example";
 
 import Logo1 from "../assets/logo1.png";
 
@@ -13,12 +14,12 @@ const Navbar = () => {
   }
 
   return (
-    <div className="fixed w-full h-[80px]  flex justify-between align-middle items-center px-4 bg-white opacity-15 text-[#0a192f] ">
+    <div className="fixed w-full h-[80px]  flex justify-between align-middle items-center px-4 bg-white opacity-15 text-[#0a192f]">
       <div className="mt-[15px]">
         <img src={Logo1} alt="Logo Image" style={{ width: "90px" }} />
       </div>
       {/* Menu */}
-      <ul className="hidden md:flex">
+      <ul className="hidden md:flex mr-[300px]">
         <li>
           <Link to="home" spy={true} smooth={true} duration={500}>
             Inicio 🗒️
@@ -35,58 +36,8 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
-
-      <Night />
-
-      {/* Hamburguer */}
-      <div onClick={handleClick} className="md:hidden z-10 ">
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-      {/* Mobile Menu */}
-      <div>
-        <ul
-          className={
-            !nav
-              ? "hidden"
-              : "absolute top-0 left-0 w-full h-screen bg-white   flex flex-col justify-center items-center "
-          }
-        >
-          <li className="py-6 text-4xl text-[black] ">
-            <Link
-              onClick={handleClick}
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Inicio 🗒️
-            </Link>
-          </li>
-          <li className="py-6 text-4xl text-[black]">
-            <Link
-              onClick={handleClick}
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Nosotros 🏗️
-            </Link>
-          </li>
-          <li className="py-6 text-4xl text-[black] ">
-            <Link
-              onClick={handleClick}
-              to="contact"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              Contacto 📲
-            </Link>
-          </li>
-        </ul>
-      </div>
       {/* Social Icons */}
+      <Example />
     </div>
   );
 };
